12b2575fe1228e1f73fe3275caec34df8762f98d
Nnamdi222 <Eneje22@gmail.com>
2023-11-11 15:17:54 -0500

    Add files via upload

---
+++ SD Knowledge Check 1.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace KnowledgeCheck1_Calculator
+    public class Calculator
+    
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+   public double divide(double first, double second)
+        {
+            
+            return first / second;
+        }
+    
